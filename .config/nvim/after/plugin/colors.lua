8ec8595c5b6e9359180e2180861c556ea6eb9e8c
pFragga <fraggatzis@gmail.com>
2023-06-19 02:48:07 +0300

    added toggle light/dark bg functionality

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,1 +1,10 @@
--- vim.cmd.colorscheme('solarized')
+function invert_bg()
+	if vim.o.background == 'dark' then
+		vim.o.background = 'light'
+	else
+		vim.o.background = 'dark'
+	end
+end
+vim.keymap.set('n', '<leader>x', invert_bg)
+
+vim.cmd.colorscheme('solarized')

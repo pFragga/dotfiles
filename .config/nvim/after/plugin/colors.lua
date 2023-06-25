79a8866bf4ba8f00c32653bc0ac0a105fcd87b78
pFragga <fraggatzis@gmail.com>
2023-06-25 22:46:31 +0300

    global function name was in lowercase

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,10 +1,10 @@
-function invert_bg()
+function Invert_bg()
 	if vim.o.background == 'dark' then
 		vim.o.background = 'light'
 	else
 		vim.o.background = 'dark'
 	end
 end
-vim.keymap.set('n', '<leader>x', invert_bg)
+vim.keymap.set('n', '<leader>x', Invert_bg)
 
 vim.cmd.colorscheme('solarized')

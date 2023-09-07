e745452ad551e0485cdbda8d8c09537b673787fe
Peter Frangatzis <pfraggatzis2002@gmail.com>
2023-09-07 13:16:55 +0300

    Revert "ignore this". I forgot to add some files
    This reverts commit 858dcd92b921dde8079f818c88b2a16ec06c9572.

--- scripts/.scripts/screenshot.sh
+++ scripts/.scripts/screenshot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-maim "$@" "$HOME"/Pictures/Screenshots/"$(date +'%F_%H-%M-%S')".png || exit 1
+maim "$@" "$HOME"/Pictures/Screenshots/maim_"$(date +'%s_%F')".png || exit 1
 notify-send "$(basename "$0")" 'Screen capture successful'

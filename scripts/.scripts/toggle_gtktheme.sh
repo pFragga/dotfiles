2c431539d9c6d46a047fc4a16d4465c225fbb76e
Peter Frangatzis <pfraggatzis2002@gmail.com>
2023-09-07 13:22:35 +0300

    going to manually run this from now on

--- scripts/.scripts/toggle_gtktheme.sh
+++ scripts/.scripts/toggle_gtktheme.sh
@@ -3,10 +3,9 @@
 gtk_settings="${XDG_CONFIG_HOME:-"$HOME/.config"}"/gtk-3.0/settings.ini
 [ -f "$gtk_settings" ] || exit 1
 
-hours=$(date +'%H')
 str='gtk-application-prefer-dark-theme'
 
-if [ "$hours" -gt 7 ] && [ "$hours" -lt 19 ];
+if grep -q "$str=1" "$gtk_settings";
 then
 	sed -i "s/$str=1/$str=0/" "$gtk_settings"
 else

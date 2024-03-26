35a18bed6e4d79820d36eedefbcfdd411cc6274c
Peter Frangatzis <pfraggatzis2002@gmail.com>
2024-03-26 15:47:54 +0200

    scripts: naming convention + functionality
    I renamed my scripts according to GitHub's shell scripting style guide.

---
+++ scripts/.scripts/leap.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# This file is meant to be sourced.
+
+depth=1
+
+case $1 in
+	"$HOME"/Documents/AUEB)
+		depth=4
+		;;
+	"$HOME"/Programming|"$HOME"/Documents/Markdown)
+		depth=2
+		;;
+esac
+
+cd "$1" || exit 1
+target=$(find . -maxdepth "$depth" -type d | fzf --prompt 'Which directory? ')
+
+if [ "$target" ]; then
+	cd "$target" || exit
+else
+	cd - > /dev/null 2>&1 || exit
+fi
